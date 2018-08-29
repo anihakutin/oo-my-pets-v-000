778e8236dc7252417327212641e24f684d1659b6
Heshie Brody <4hbrody@gmail.com>
2018-08-29 23:18:44 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -32,7 +32,7 @@ class Owner
     pets[:fishes].each {|f| f.mood = "happy"}
   end
   def sell_pets
-    @pets.each do |species, pets|
+    pets.each do |species, pets|
       pets.each {|p| p.mood = "nervous"}
     end
     pets.clear
